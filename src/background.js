f5a66ea57035a83614d855a06a1eecc8f2e59cbe
Luís Dalmolin <luis.nh@gmail.com>
2020-08-21 14:44:36 -0300

    First commit

---
+++ src/background.js
@@ -0,0 +1,80 @@
+'use strict'
+
+import { app, protocol, BrowserWindow } from 'electron'
+import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
+import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
+import { autoUpdater } from "electron-updater"
+import { menubar } from 'menubar';
+import { join } from 'path';
+
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
+
+// Scheme must be registered before the app is ready
+protocol.registerSchemesAsPrivileged([
+    { scheme: 'app', privileges: { secure: true, standard: true } }
+])
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', async () => {
+    if (isDevelopment && !process.env.IS_TEST) {
+        // Install Vue Devtools
+        try {
+            await installExtension(VUEJS_DEVTOOLS)
+        } catch (e) {
+            console.error('Vue Devtools failed to install:', e.toString())
+        }
+    }
+
+    if (! process.env.WEBPACK_DEV_SERVER_URL) {
+        createProtocol('app')
+    }
+
+    const bar = menubar({
+        index: process.env.WEBPACK_DEV_SERVER_URL
+            ? process.env.WEBPACK_DEV_SERVER_URL
+            : 'app://./index.html',
+
+        browserWindow: {
+            alwaysOnTop: true,
+            webPreferences: {
+                nodeIntegration: true,
+            }
+        },
+        icon: join(__static, 'IconTemplate.png')
+    });
+
+    mb.on('ready', () => {
+        mb.showWindow();
+    })
+})
+
+// Exit cleanly on request from parent process in development mode.
+if (isDevelopment) {
+    if (process.platform === 'win32') {
+        process.on('message', (data) => {
+            if (data === 'graceful-exit') {
+                app.quit()
+            }
+        })
+    } else {
+        process.on('SIGTERM', () => {
+            app.quit()
+        })
+    }
+}
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex items-center justify-center h-full">
+        <h1 class="text-2xl">{{ msg }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld',
+    props: {
+        msg: String
+    }
+}
+</script>
