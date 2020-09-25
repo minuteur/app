63d5e11dd54d9906da78bbe238435384e47c424c
Luís Dalmolin <luis.nh@gmail.com>
2020-09-25 16:09:56 -0300

    Time editing

--- src/components/pages/Sessions.vue
+++ src/components/pages/Sessions.vue
@@ -31,7 +31,8 @@
                     </div>
 
                     <!-- Session edit overlay -->
-                    <div class="absolute w-full bottom-0 left-0 px-6 py-4 bg-gray-800 shadow border-t-4 border-t-gray-700" v-if="state === 'edit'">
+                    <div class="fixed w-full h-full top-0 left-0 bg-gray-900 opacity-50 overflow-hidden" v-if="state === 'edit'"></div>
+                    <div class="fixed w-full bottom-0 left-0 px-6 py-4 bg-gray-800 shadow border-t-4 border-t-gray-700" v-if="state === 'edit'">
                         <div class="mb-3">
                             <label for="name" class="block text-sm font-medium leading-5 text-white">Session name</label>
                             <div class="mt-1 relative rounded-md shadow-sm">
@@ -46,21 +47,55 @@
                             </div>
                         </div>
 
-                        <div>
-                            <label for="date" class="block text-sm font-medium leading-5 text-white">Date</label>
-                            <div class="mt-1 relative rounded-md shadow-sm">
-                                <input
-                                    id="date"
-                                    class="form-input form-input-sm block w-full sm:text-sm sm:leading-5"
-                                    placeholder="YYYY-MM-DD"
-                                    type="date"
-                                    ref="date"
-                                    v-model="edit.session.date"
-                                    @keydown.enter="update"
-                                >
+                        <div class="flex justify-between">
+                            <div>
+                                <label for="date" class="block text-sm font-medium leading-5 text-white">Date</label>
+                                <div class="mt-1 relative rounded-md shadow-sm">
+                                    <input
+                                        id="date"
+                                        class="form-input form-input-sm block w-full sm:text-sm sm:leading-5"
+                                        placeholder="YYYY-MM-DD"
+                                        type="date"
+                                        ref="date"
+                                        v-model="edit.session.date"
+                                        @keydown.enter="update"
+                                    >
+                                </div>
+                            </div>
+
+                            <div>
+                                <label for="hours" class="block text-sm font-medium leading-5 text-white">Time</label>
+                                <div class="flex mt-1 relative rounded-md shadow-sm">
+                                    <input
+                                        id="hours"
+                                        class="form-input form-input-sm block w-12 sm:text-sm sm:leading-5 mr-2"
+                                        placeholder="H"
+                                        type="text"
+                                        v-model="edit.session.hours"
+                                        @keydown.enter="update"
+                                    >
+
+                                    <input
+                                        id="minutes"
+                                        class="form-input form-input-sm block w-12 sm:text-sm sm:leading-5 mr-2"
+                                        placeholder="M"
+                                        type="text"
+                                        v-model="edit.session.minutes"
+                                        @keydown.enter="update"
+                                    >
+
+                                    <input
+                                        id="seconds"
+                                        class="form-input form-input-sm block w-12 sm:text-sm sm:leading-5"
+                                        placeholder="S"
+                                        type="text"
+                                        v-model="edit.session.seconds"
+                                        @keydown.enter="update"
+                                    >
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </div><!-- Edit overlay -->
                 </div>
 
                 <footer class="flex justify-between px-6 py-2 bg-gray-800 text-white">
@@ -72,8 +107,9 @@
 </template>
 
 <script>
-import Session from '@models/Session'
-import { SESSION_STATUS_DONE, SESSION_STATUS_RUNNING } from '@models/Session'
+import TimeManager from '@lib/TimeManager';
+import Session from '@models/Session';
+import { SESSION_STATUS_DONE, SESSION_STATUS_RUNNING } from '@models/Session';
 import Layout from '@components/Layout';
 import SessionRow from '@components/SessionRow';
 
@@ -119,6 +155,9 @@ export default {
 
         async onSessionEdit (index) {
             this.edit.session = this.sessions[index];
+            this.edit.session.hours = TimeManager.hoursFromSeconds(this.edit.session.time);
+            this.edit.session.minutes = TimeManager.minutesFromSeconds(this.edit.session.time);
+            this.edit.session.seconds = TimeManager.secondsFromSeconds(this.edit.session.time);
             this.state = 'edit';
 
             this.$nextTick(() => {
@@ -127,11 +166,20 @@ export default {
         },
 
         async update () {
+            let time = TimeManager.convertToSeconds(
+                this.edit.session.hours,
+                this.edit.session.minutes,
+                this.edit.session.seconds,
+            );
+
+            this.edit.session.time = time;
+
             await Session.update(
                 this.edit.session.uuid,
                 {
                     name: this.edit.session.name,
-                    date: this.edit.session.date
+                    date: this.edit.session.date,
+                    time: time
                 }
             );
 
--- src/lib/TimeManager.js
+++ src/lib/TimeManager.js
@@ -3,14 +3,52 @@ import moment from 'moment';
 class TimeManager {
     /**
      * Get the data diff in seconds.
-     * @param string date
+     * @param {string} date
      */
     toSeconds (date) {
         return moment().diff(moment(date), 'seconds');
     }
 
+    /**
+     * Get the time in seconds and convert to H:i:s
+     * @param {int} totalSeconds
+     */
     toFormattedTime (totalSeconds) {
         return new Date(totalSeconds * 1000).toISOString().substr(11, 8);
+    }
+
+    /**
+     * Get the number of hours for a given number of seconds in a hour:minute:seconds perspective.
+     * @param {int} totalSeconds
+     */
+    hoursFromSeconds (totalSeconds) {
+        return new Date(totalSeconds * 1000).toISOString().substr(11, 2);
+    }
+
+    /**
+     * Get the number of minutes for a given number of seconds in a hour:minute:seconds perspective.
+     * @param {int} totalSeconds
+     */
+    minutesFromSeconds (totalSeconds) {
+        return new Date(totalSeconds * 1000).toISOString().substr(14, 2);
+    }
+
+    /**
+     * Get the number of seconds for a given number of seconds in a hour:minute:seconds perspective.
+     * @param {int} totalSeconds
+     */
+    secondsFromSeconds (totalSeconds) {
+        return new Date(totalSeconds * 1000).toISOString().substr(17, 2);
+    }
+
+    /**
+     * Convert hours, minutes and seconds to a full sum of seconds.
+     * @param {int} hours
+     * @param {int} minutes
+     * @param {int} seconds
+     */
+    convertToSeconds (hours, minutes, seconds) {
+        return (parseInt(hours) * 60 * 60) + (parseInt(minutes) * 60) + (parseInt(seconds));
     }
 }
 
